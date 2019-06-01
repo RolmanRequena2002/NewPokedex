c2523f842ba819666b6280af3112c930a81d6165
RolmanRequena2002 <rolreque2002@gmail.com>
2019-06-01 16:31:33 -0600

    POKE

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Write your code here
+// Write your cod
+var Pokemon= angular.module("Pokemon",[])
+Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Lista = [];
+	for (var y = 1; y <= 1000; y++) {
+		$http({
+			method:"GET",
+			url:"https://pokeapi.co/api/v2/pokemon/"+y
+		}).then(function datos(x){
+			$scope.Lista.push(x);
+		});
+	}
+});
